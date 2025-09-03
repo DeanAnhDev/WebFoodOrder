adab67994518b38e39c87a733caf4c9a1dcad44e
DeanAnhDev <dinhtheanhts29012005@gmail.com>
2025-09-03 17:30:43 +0700

    update voucher

--- FoodOrder/FoodOrder.Application/Services/Orders/VoucherService.cs
+++ FoodOrder/FoodOrder.Application/Services/Orders/VoucherService.cs
@@ -47,13 +47,13 @@ namespace FoodOrder.Application.Services.Orders
             if (entity.MaxDiscountPrice < 0)
                 throw new ArgumentException("Giá trị giảm tối đa không được < 0");
 
-            if (entity.StartDate < DateTime.UtcNow)
+            if (entity.StartDate < DateTime.Now.Date)
                 throw new ArgumentException("Ngày bắt đầu phải lớn hơn hoặc bằng ngày hiện tại");
 
-            if (entity.StartDate >= entity.EndDate)
-                throw new ArgumentException("Ngày bắt đầu phải nhỏ hơn ngày kết thúc");
+            if (entity.StartDate > entity.EndDate)
+                throw new ArgumentException("Ngày bắt đầu phải nhỏ hơn hoặc bằng ngày kết thúc");
 
-            if (entity.EndDate < DateTime.UtcNow)
+            if (entity.EndDate.Date < DateTime.Now.Date)
                 throw new ArgumentException("Ngày kết thúc phải lớn hơn hoặc bằng ngày hiện tại");
 
             var voucher = _mapper.Map<Voucher>(entity);
@@ -172,14 +172,12 @@ namespace FoodOrder.Application.Services.Orders
             if (entity.MaxDiscountPrice < 0)
                 throw new ArgumentException("Giá trị giảm tối đa không được < 0");
 
-            if (entity.StartDate < DateTime.UtcNow)
-                throw new ArgumentException("Ngày bắt đầu phải lớn hơn hoặc bằng ngày hiện tại");
+            if (entity.StartDate > entity.EndDate)
+                throw new ArgumentException("Ngày bắt đầu phải nhỏ hơn hoặc bằng ngày kết thúc");
 
-            if (entity.StartDate >= entity.EndDate)
-                throw new ArgumentException("Ngày bắt đầu phải nhỏ hơn ngày kết thúc");
-
-            if (entity.EndDate < DateTime.UtcNow)
+            if (entity.EndDate.Date < DateTime.Now.Date)
                 throw new ArgumentException("Ngày kết thúc phải lớn hơn hoặc bằng ngày hiện tại");
+
 
 
             var voucher = _mapper.Map(entity, existing);
